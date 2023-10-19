5f971931ae764ad7673ba0f0aaf00ac54de06ef8
Mammadali Gasimov <mamed.gasymov@gmail.com>
2023-10-19 16:31:55 +0400

    get cv validation param userid

--- src/middleware/validation/paramId.ts
+++ src/middleware/validation/paramId.ts
@@ -3,3 +3,7 @@ import { param } from 'express-validator';
 export const paramIdValidationSchema = [
   param('id').escape().notEmpty().withMessage('"id" is required').isInt({ gt: 0 }),
 ];
+
+export const paramUserIdValidationSchema = [
+  param('userId').escape().notEmpty().withMessage('"userId" is required').isInt({ gt: 0 }),
+];
--- src/routes/cv.ts
+++ src/routes/cv.ts
@@ -3,11 +3,12 @@ import express from 'express';
 import { Context, RouterFactory } from '@interfaces/general';
 import { logRequestId } from '@middleware/logger/logRequestId';
 import { getCVController } from '@controllers/index';
+import { paramUserIdValidationSchema } from '@middleware/validation/paramId';
 
 export const makeCVRouter: RouterFactory = (context: Context) => {
   const router = express.Router();
 
-  router.get('/:userId/cv', logRequestId, getCVController(context));
+  router.get('/:userId/cv', logRequestId, paramUserIdValidationSchema, getCVController(context));
 
   return router;
 };
