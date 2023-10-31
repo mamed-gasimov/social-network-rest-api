e6fe5cb0a0747f1d55ea6bea166a6fc019c7daad
Mammadali Gasimov <mamed.gasymov@gmail.com>
2023-10-31 14:07:16 +0400

    add ttypescript package, and prod build and start commands

--- src/config/index.ts
+++ src/config/index.ts
@@ -20,6 +20,7 @@ export interface Config {
 const configs: {
   development: Config;
   test: Config;
+  production: Config;
 } = {
   development: {
     db: {
@@ -53,23 +54,44 @@ const configs: {
       secret: 'some-dev-secret',
     },
   },
+  production: {
+    db: {
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_USER_PASSWORD,
+      database: 'capstone_project',
+    },
+    redis: {
+      host: 'localhost',
+      port: 6379,
+    },
+    auth: {
+      secret: 'some-dev-secret',
+    },
+  },
 };
 
 const getConfig = (): Config => {
   configEnv();
 
   if (!process.env.NODE_ENV) {
-    throw new Error('Env parameter NODE_ENV must be specified! Possible values are "development", "test" ...');
+    throw new Error(
+      'Env parameter NODE_ENV must be specified! Possible values are "development", "test", "production"',
+    );
   }
 
   const env = process.env.NODE_ENV as 'development';
 
   if (!configs[env]) {
-    throw new Error('Unsupported NODE_ENV value was provided! Possible values are "development", "test", ...');
+    throw new Error('Unsupported NODE_ENV value was provided! Possible values are "development", "test", "production"');
   }
 
   configs.development.db.username = process.env.DB_USER_NAME;
   configs.development.db.password = process.env.DB_USER_PASSWORD;
+
+  configs.production.db.username = process.env.DB_USER_NAME;
+  configs.production.db.password = process.env.DB_USER_PASSWORD;
 
   return configs[env];
 };
